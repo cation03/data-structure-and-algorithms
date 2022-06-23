c16ae00ec5ca3f0d25031c4d741671cf74f0b088
Ananya Goyal <78123938+cation03@users.noreply.github.com>
2022-06-23 12:58:00 +0530

    Added BST Inorder Functions

--- bst.c
+++ bst.c
@@ -2,15 +2,51 @@
 #include <stdlib.h>
 typedef struct item {
     int data;
-    struct item * right;
-    struct item * left;
+    struct item * right, *left;
 } item;
 
-int isEmpty()
+item* createNewNode(int key){
+    item * temp = (item *)malloc(sizeof(item));
+    temp->data = key;
+    temp->left = temp->right = NULL;
+    return temp;
+}
 
+void inorder(item* root){
+    if (root != NULL){
+        inorder(root->left);
+        printf("%d \n", root->data);
+        inorder(root->right);
+    }
+}
+
+item* insert(item* node, int key){
+    if (node == NULL){
+        return createNewNode(key);
+    }
+
+    if (key < node->data){
+        node->left = insert(node->left, key);
+    }
+    else if (key > node->data){
+        node->right = insert(node->right, key);
+    }
+    return node;
+    
+}
 
 int main()
 {
+    item * root = NULL;
+    root = insert(root, 50);
+    insert(root, 30);
+    insert(root, 20);
+    insert(root, 40);
+    insert(root, 70);
+    insert(root, 60);
+    insert(root, 80);
+    
+    inorder(root);
 
 
     return 0;
