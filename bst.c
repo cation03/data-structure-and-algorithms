b8d3bd430e66088b3900557c1aba1a7383a9897b
Ananya Goyal <78123938+cation03@users.noreply.github.com>
2022-06-23 13:44:15 +0530

    Added delete node function

--- bst.c
+++ bst.c
@@ -35,19 +35,86 @@ item* insert(item* node, int key){
     
 }
 
+item * search(item* root, int key){
+    if (root == NULL || root->data == key){
+        return root;
+    }
+    if (root->data > key){
+        return search(root->left, key);
+    }
+    return search(root->left, key);
+}
+
+item* minValue(item* node){
+    item* current = node;
+
+    while (current && current->left != NULL){
+        current = current->left;
+    }
+    return current;
+}
+
+item* deleteNode(item* node, int key){
+    if (node == NULL){
+        return node;
+    }
+    else if (key < node->data){
+        return deleteNode(node->left, key);
+    }
+    else if (key > node->data){
+        return deleteNode(node->right, key);
+    }
+    else {
+        if (node->left == NULL && node->right == NULL){
+            return NULL;
+        }
+        else if (node->left == NULL){
+            item * temp = node->right;
+            free(node);
+            return temp;
+        }
+        else if (node->right == NULL){
+            item * temp = node->left;
+            free(node);
+            return temp;
+        }
+        item* temp = minValue(node->right);
+        node->data = temp->data;
+        node->right = deleteNode(node->right, temp->data);
+    }
+    return node;
+
+}
+
 int main()
 {
     item * root = NULL;
     root = insert(root, 50);
-    insert(root, 30);
-    insert(root, 20);
-    insert(root, 40);
-    insert(root, 70);
-    insert(root, 60);
-    insert(root, 80);
+    root = insert(root, 30);
+    root = insert(root, 20);
+    root = insert(root, 40);
+    root = insert(root, 70);
+    root = insert(root, 60);
+    root = insert(root, 80);
+
+    printf("Inorder traversal: \n");
     
     inorder(root);
 
+    printf("On deleting 20: \n");
+    root = deleteNode(root, 20);
+    printf("Inorder traversal now: \n");
+    inorder(root);
+
+    printf("On deleting 30: \n");
+    root = deleteNode(root, 30);
+    printf("Inorder traversal now: \n");
+    inorder(root);
+
+    printf("On deleting 50: \n");
+    root = deleteNode(root, 50);
+    printf("Inorder traversal now: \n");
+    inorder(root);
 
     return 0;
 }
